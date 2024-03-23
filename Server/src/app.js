591d69f775f32fcbe64e9ab7b3623142b7691a3f
modycheif <ankitpandey200154@gmail.com>
2024-03-23 12:24:01 +0530

    Setting up

--- Server/src/app.js
+++ Server/src/app.js
@@ -15,4 +15,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+app.get('/',(req,res)=>{
+    res.send('<h1>Hey SERVER!</h1>');
+})
+
 export { app };
